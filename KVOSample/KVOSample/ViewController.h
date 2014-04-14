863d47fbb8637b4f30faa1f5a1e2e3bb89975ec2
leesongfan <781957672@qq.com>
2014-04-14 10:16:57 +0800

    KVO

---
+++ KVOSample/KVOSample/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  KVOSample
+//
+//  Created by 范林松 on 14-4-11.
+//  Copyright (c) 2014年 leesong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ThemeManager.h"
+
+@interface ViewController : UIViewController<UIActionSheetDelegate>
+
+
+-(IBAction)didClickChangeTheme:(id)sender;
+
+@end
---
+++ KVOSample/KVOSampleTests/KVOSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  KVOSampleTests.m
+//  KVOSampleTests
+//
+//  Created by 范林松 on 14-4-11.
+//  Copyright (c) 2014年 leesong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KVOSampleTests : XCTestCase
+
+@end
+
+@implementation KVOSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
